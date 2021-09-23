a24ed185948cd87ae64c9cbabd9986a4a181aedf
Your Name <you@example.com>
2021-09-23 10:32:55 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Link, Route } from "react-router-dom";
-import HomePage from './comps/HomePage';
+// import HomePage from './comps/HomePage';
+import Office from './comps/Office';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      <Route path="/" component={HomePage}/>
-      
+      {/* <Route path="/" component={HomePage}/> */}
+      <Office/>
       </div>
 
     </BrowserRouter>
---
+++ src/comps/Office.js
@@ -0,0 +1,92 @@
+import React from "react";
+import "./Office.css"
+
+function Office(){
+    return(
+        <div className="flow">
+            
+
+
+
+
+            <div className="moduleT">
+                
+                <div className="d1">PERSON 1</div>
+                <div className="d2"> PERSON 2</div>
+                <div className="d3">PERSON 3</div>
+                <div className="d4">PERSON 4</div>
+                <div className="d4">PERSON 5</div>
+                <div className="out">
+                <button className="start2">Join In</button>
+                <button className="start2">Leave</button>
+                </div>
+                
+
+
+
+            </div>
+            <div className="moduleG">
+                <div className="d1">PERSON 1</div>
+                <div className="d2">PERSON 2</div>
+                <div className="d3">PERSON 3</div>
+                <div className="d4">PERSON 4</div>
+                <div className="d4">PERSON 5</div>
+                <div className="out">
+                <button className="start2">Join in</button>
+                <button className="start2">Leave</button>
+                </div>
+               
+
+
+
+            </div>
+            <div className="moduleF">
+                <div className="d1">PERSON 1</div>
+                <div className="d2">PERSON 2</div>
+                <div className="d3">PERSON 3</div>
+                <div className="d4">PERSON 4</div>
+                <div className="d4">PERSON 5</div>
+                <div className="out">
+                <button className="start2">Join in</button>
+                <button className="start2">Leave</button>
+                </div>
+                
+
+
+
+                
+                
+                
+                
+                
+
+            </div>
+            
+            <div className="moduleM">
+                <div  className="d1">PERSON 1</div>
+                <div  className="d2">PERSON 2</div>
+                <div  className="d3">PERSON 3</div>
+                <div  className="d4">PERSON 4</div>
+                <div  className="d4">PERSON 5</div>
+                <div className="out">
+                <button className="start2">Join in</button>
+                <button className="start2">Leave</button>
+                </div>
+                
+
+                
+                
+
+
+
+            </div>
+
+  
+
+            
+        </div>
+    )
+}
+
+
+export default Office;
